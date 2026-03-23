26a7fcc0fea0dad6f9129508e4693d982ccdb7a0
NicoRuedaA <nico99rueda@gmail.com>
2026-03-23 16:39:10 +0100

    23/03/26

--- JS/components.js
+++ JS/components.js
@@ -12,13 +12,16 @@ class MiHeader extends HTMLElement {
        <iframe id="fondo-aliens" src="background.html"></iframe>
     <header>
       <nav class="topBar">
+   
+        <a href="index.html">✳</a>
         <a href="index.html">Inicio </a>
         <a href="about-me.html"> Sobre mi</a>
         <a href="decks.html"> Decks </a>
         <a href="https://github.com/NicoRuedaA" target="_blank"><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/nicolas-rueda-araque-737241199/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-
       </nav>
+
+      
     </header>
         `;
   }
