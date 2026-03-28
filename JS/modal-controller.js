abe8a8a642670357d662f20cda03aa386ef92c3a
Nico <nico99rueda@gmail.com>
2026-03-28 22:33:44 +0100

    Pre release

--- JS/modal-controller.js
+++ JS/modal-controller.js
@@ -4,15 +4,10 @@ const btnCerrar = document.getElementById("btnCerrar");
 
 // Abrir el modal
 btnAbrir.addEventListener("click", () => {
-  modal.showModal(); // Importante: showModal() habilita el fondo oscuro
+  modal.showModal(); // showModal() habilita el fondo oscuro
 });
 
 // Cerrar el modal
 btnCerrar.addEventListener("click", () => {
   modal.close();
-});
-
-// Cerrar al hacer clic fuera del contenido (en el backdrop)
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) modal.close();
 });
